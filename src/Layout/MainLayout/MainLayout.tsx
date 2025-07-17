0405fabf1273fd9b16132644beed3c107bccfb54
isLah-15 <alexiskish4146@gmail.com>
2025-07-17 14:04:13 +0300

    Added a footer

--- src/Layout/MainLayout/MainLayout.tsx
+++ src/Layout/MainLayout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import Navbar from "../../Components/Nav/Navbar";
 
+
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen bg-neutral-100">
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,18 @@
+import Footer from "../Components/Footer/FooterSection";
 import HeroSection from "../Components/Home/Hero";
 import MainLayout from "../Layout/MainLayout/MainLayout";
 
 
 export default function Home() {
   return (
+    <div>
     <MainLayout>
       <HeroSection />
     </MainLayout>
+    <Footer />
+   
+
+    </div>
+
   );
 }
